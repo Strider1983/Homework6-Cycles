1805da02ab85417915829555a838ff32a00ead2d
Strider <mezentsev.k@gmail.com>
2023-08-14 19:42:17 +0800

    Задача №2 выполнена

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,10 @@ public class Main {
         for (int i = 1; i <= 10; i = i + 1) {
             System.out.println(i);
         }
+        System.out.println("__________________");
+        System.out.println("Задача №2");
+        for (int g = 10; g > 0; g = g - 1) {
+            System.out.println(g);
+        }
     }
 }
