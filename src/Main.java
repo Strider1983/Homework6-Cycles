79c4494b984bf42350dbb03e1e8770d321912016
Strider <mezentsev.k@gmail.com>
2023-08-14 19:40:01 +0800

    Задача №1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Циклы. Урок №1");
+        System.out.println("__________________");
+        System.out.println("Задача №1");
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println(i);
+        }
     }
 }
